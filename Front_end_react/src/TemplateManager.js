90e4d2f0ca1ed391bfb366544857a3b1630bb156
karin478 <79677735+karin478@users.noreply.github.com>
2023-12-13 12:00:16 +0000

    Update TemplateManager.js

--- Front_end_react/src/TemplateManager.js
+++ Front_end_react/src/TemplateManager.js
@@ -6,7 +6,7 @@ const TemplateManager = () => {
 
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:8000/api/weekend_template/")
+      .get("http://web:8000/api/weekend_template/")
       .then((response) => {
         setTemplates(response.data.results);
       })
